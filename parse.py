b58dcaf7ba203d36fb8fa58f5628fd08be0c508a
everaldorodrigo <everaldorodrigo@gmail.com>
2023-10-26 11:42:13 -0700

    Add _id field.

--- parse.py
+++ parse.py
@@ -7,7 +7,7 @@ def load_data(data_folder):
     with open(json_path) as f:
         for line in f:
             datum = json.loads(line)
-            datum['_id'] = datum['accession_id']
+            datum['_id'] = datum['sra_accession']
             yield datum
 
 
