c5d2b7c4469caece5dbb8f8c912c0b335800af3b
everaldorodrigo <everaldorodrigo@gmail.com>
2024-01-08 15:28:41 -0800

    Add variants data files.

--- parse.py
+++ parse.py
@@ -2,7 +2,7 @@ import json
 import os
 
 
-def load_data(data_folder):
+def load_data_aggregate_demix(data_folder):
     json_path = os.path.join(data_folder, "aggregate_demix.json")
     with open(json_path) as f:
         for line in f:
@@ -11,7 +11,25 @@ def load_data(data_folder):
             yield datum
 
 
-def custom_data_mapping(cls):
+def load_data_aggregate_variants_by_acc(data_folder):
+    json_path = os.path.join(data_folder, "aggregate_variants_by_acc.json")
+    with open(json_path) as f:
+        for line in f:
+            datum = json.loads(line)
+            datum['_id'] = datum['sra_accession']
+            yield datum
+
+
+def load_data_aggregate_variants_by_mut(data_folder):
+    json_path = os.path.join(data_folder, "aggregate_variants_by_mut.json")
+    with open(json_path) as f:
+        for line in f:
+            datum = json.loads(line)
+            datum['_id'] = datum['mut_hash']
+            yield datum
+
+
+def custom_data_mapping_aggregate_demix(cls):
     return {
         "sra_accession": {"type": "keyword"},
         "lineages": {
@@ -42,4 +60,94 @@ def custom_data_mapping(cls):
         "ww_population": {"type": "double"},
         "viral_load": {"type": "double"},
         "site_id": {"type": "keyword"},
+    }
+
+
+def custom_data_mapping_aggregate_variants_by_acc(cls):
+    return {
+        "sra_accession": {
+            "type": "keyword",
+            "normalizer": "keyword_lowercase_normalizer"
+        },
+        "mutations": {
+            "type": "nested",
+            "properties": {
+                "mut_name": {
+                    "type": "keyword",
+                    "normalizer": "keyword_lowercase_normalizer"
+                },
+                "frequency": {
+                    "type": "double"
+                },
+                "depth": {
+                    "type": "double"
+                }
+            }
+        },
+        "collection_date": {
+            "type": "keyword",
+            "normalizer": "keyword_lowercase_normalizer"
+        },
+        "geo_loc_country": {
+            "type": "keyword",
+            "normalizer": "keyword_lowercase_normalizer"
+        },
+        "geo_loc_region": {
+            "type": "keyword",
+            "normalizer": "keyword_lowercase_normalizer"
+        },
+        "ww_population": {
+            "type": "double"
+        },
+        "viral_load": {
+            "type": "double"
+        },
+        "site_id": {
+            "type": "keyword"
+        }
+    }
+
+
+def custom_data_mapping_aggregate_variants_by_mut(cls):
+    return {
+        "mut_hash": {
+            "type": "keyword"
+        },
+        "mut_name": {
+            "type": "keyword"
+        },
+        "samples": {
+            "type": "nested",
+            "properties": {
+                "sra_accession": {
+                    "type": "keyword"
+                },
+                "frequency": {
+                    "type": "double"
+                },
+                "depth": {
+                    "type": "double"
+                },
+                "collection_date": {
+                    "type": "keyword"
+                },
+                "geo_loc_country": {
+                    "type": "keyword",
+                    "normalizer": "keyword_lowercase_normalizer"
+                },
+                "geo_loc_region": {
+                    "type": "keyword",
+                    "normalizer": "keyword_lowercase_normalizer"
+                },
+                "ww_population": {
+                    "type": "double"
+                },
+                "viral_load": {
+                    "type": "double"
+                },
+                "site_id": {
+                    "type": "keyword"
+                }
+            }
+        }
     }
--- version.py
+++ version.py
@@ -1,5 +1,14 @@
 def get_release(self):
     import requests
 
-    res = requests.head("https://storage.googleapis.com/outbreak-ww-data/aggregate/aggregate_demix.json")
-    return res.headers["Last-Modified"]
+    res_demix = requests.head("https://storage.googleapis.com/outbreak-ww-data/aggregate/aggregate_demix.json")
+    res_variants_by_acc = requests.head("https://storage.googleapis.com/outbreak-ww-data/aggregate/aggregate_variants_by_acc.json")
+    res_variants_by_mut = requests.head("https://storage.googleapis.com/outbreak-ww-data/aggregate/aggregate_variants_by_mut.json")
+
+    version = "_".join([
+        res_demix.headers["Last-Modified"],
+        res_variants_by_acc.headers["Last-Modified"],
+        res_variants_by_mut.headers["Last-Modified"]
+    ])
+
+    return version
