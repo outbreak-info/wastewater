a6269777254c01878687975d61a13786ece7f38d
everaldorodrigo <everaldorodrigo@gmail.com>
2024-01-08 16:02:07 -0800

    Add variants data files.

--- version.py
+++ version.py
@@ -5,7 +5,7 @@ def get_release(self):
     res_variants_by_acc = requests.head("https://storage.googleapis.com/outbreak-ww-data/aggregate/aggregate_variants_by_acc.json")
     res_variants_by_mut = requests.head("https://storage.googleapis.com/outbreak-ww-data/aggregate/aggregate_variants_by_mut.json")
 
-    version = " # ".join([
+    version = " ".join([
         "demix on " + res_demix.headers["Last-Modified"],
         "acc on " + res_variants_by_acc.headers["Last-Modified"],
         "mut on " + res_variants_by_mut.headers["Last-Modified"]
